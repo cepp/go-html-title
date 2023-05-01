39cc179e0e985d1929e24b9a5129f286879de90d
Carlos Pantoja <ceppantoja@gmail.com>
2023-05-01 10:47:11 -0300

    versão inicial

---
+++ go-html-title.go
@@ -0,0 +1,26 @@
+package gohtmltitle
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+)
+
+// Google I/O 2012 - Go Concurrency Patterns
+
+//  <- chan - canal somente-leitura
+// Titulo extrai o título das urls enviadas via parâmetro
+func Titulo(urls ...string) <-chan string {
+	channel := make(chan string)
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := io.ReadAll(resp.Body)
+
+			read, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			channel <- read.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+	return channel
+}
